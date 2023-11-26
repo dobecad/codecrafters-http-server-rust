9b3825d371d371d8818c506eb8b18659afc733b2
jachii <jettymcfiggs@gmail.com>
2023-11-25 20:48:20 -0500

    3rd stage

--- src/main.rs
+++ src/main.rs
@@ -38,10 +38,33 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
         return Ok(());
     }
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = String::from_utf8_lossy(&buffer);
+    let parts: Vec<String> = contents.lines().map(|line| line.to_string()).collect();
+    let path = parts
+        .iter()
+        .nth(0)
+        .map(|start_line| start_line.split(" ").nth(1))
+        .context("missing path")?
+        .context("missing starting line")?;
+
+    match path {
+        "/" => {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            send_response(stream, response)?;
+        }
+        _ => {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            send_response(stream, response)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn send_response(mut stream: TcpStream, content: &str) -> Result<()> {
     let _ = stream
-        .write_all(response.as_bytes())
+        .write_all(content.as_bytes())
         .context("failed to write to TcpStream")?;
-    let _ = stream.flush()?;
+    let _ = stream.flush().context("failed to flush TcpStream")?;
     Ok(())
 }
